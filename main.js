49e8a622eb8f002a17c425846d61f3edffbebb1e
Joe Fenton <joe.e.fenton@gmail.com>
2017-06-08 16:06:35 -0400

    assessment

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 // Add your JS here.
+function todolist() {
+  var item = document.getelementbyid("new-todo").value
+  var text = document.createtextnode(item)
+  var newitem = document.createelement("li")
+  newitem.appendchild(text)
+  document.getelementbyid("todo-list").appendchid(newitem)
+}
